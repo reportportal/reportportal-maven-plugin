db42728478b745fe09c7166f4bf78c7916a33a29
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-12-20 17:43:44 +0300

    WIP

--- src/main/java/com/epam/reportportal/ReportPortalInstallMojo.java
+++ src/main/java/com/epam/reportportal/ReportPortalInstallMojo.java
@@ -7,15 +7,24 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import static java.util.Optional.ofNullable;
+
 @Mojo(name = "configure", defaultPhase = LifecyclePhase.GENERATE_TEST_RESOURCES)
 public class ReportPortalInstallMojo extends AbstractMojo {
 
 	@Parameter(property = "session", defaultValue = "${session}", readonly = true)
 	private MavenSession session;
 
-	public void execute() throws MojoExecutionException
-	{
-		getLog().info( "Got these plugins: " );
-		session.getCurrentProject().getBuildPlugins().forEach(p->getLog().info(p.getId()));
+	public void execute() throws MojoExecutionException {
+		getLog().info("Got these plugins: ");
+		session.getCurrentProject()
+				.getBuildPlugins()
+				.stream()
+				.filter(p -> p.getId().startsWith("org.apache.maven.plugins:maven-surefire-plugin") || p.getId()
+						.startsWith("org.apache.maven.plugins:maven-surefire-plugin"))
+				.forEach(p -> {
+					getLog().info(ofNullable(p.getConfiguration()).map(Object::toString).orElse("NULL"));
+					getLog().info("Class: " + ofNullable(p.getConfiguration()).map(Object::getClass).map(Class::getName).orElse("NULL"));
+				});
 	}
 }
