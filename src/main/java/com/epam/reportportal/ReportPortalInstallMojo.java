1441e92d890ee4313b63cc505e6c4a941d773c9a
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-12-20 15:35:08 +0300

    Test plugin get

---
+++ src/main/java/com/epam/reportportal/ReportPortalInstallMojo.java
@@ -0,0 +1,21 @@
+package com.epam.reportportal;
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "install", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+public class ReportPortalInstallMojo extends AbstractMojo {
+
+	@Parameter(property = "session", defaultValue = "${session}", readonly = true)
+	private MavenSession session;
+
+	public void execute() throws MojoExecutionException
+	{
+		getLog().info( "Got these plugins: " );
+		session.getCurrentProject().getBuildPlugins().forEach(p->getLog().info(p.getId()));
+	}
+}
