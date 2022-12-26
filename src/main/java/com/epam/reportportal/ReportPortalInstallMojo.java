db76a0513110dc09f0c569ae45976ef4b51e9cad
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-12-26 17:18:29 +0300

    `overrideProperties` method add

--- src/main/java/com/epam/reportportal/ReportPortalInstallMojo.java
+++ src/main/java/com/epam/reportportal/ReportPortalInstallMojo.java
@@ -1,5 +1,6 @@
 package com.epam.reportportal;
 
+import com.epam.reportportal.utils.properties.PropertiesLoader;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -8,32 +9,71 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
 import static java.util.Optional.ofNullable;
 
 @Mojo(name = "configure", defaultPhase = LifecyclePhase.GENERATE_TEST_RESOURCES)
 public class ReportPortalInstallMojo extends AbstractMojo {
 
+	private static final String RP_PLUGIN_ID = "com.epam.reportportal:reportportal-maven-plugin";
+
 	@Parameter(property = "session", defaultValue = "${session}", readonly = true)
 	private MavenSession session;
 
-	public void execute() throws MojoExecutionException {
-		getLog().info("Got these plugins: ");
-		session.getCurrentProject()
+	@Parameter(property = "project.build.directory", defaultValue = "${project.build.directory}", readonly = true)
+	private String buildDirectoryPath;
+
+	private void overrideProperties() throws MojoExecutionException {
+		Properties pluginProperties = new Properties();
+		pluginProperties.putAll(session.getCurrentProject()
 				.getBuildPlugins()
 				.stream()
-				.filter(p -> p.getId().startsWith("org.apache.maven.plugins:maven-surefire-plugin") || p.getId()
-						.startsWith("org.apache.maven.plugins:maven-surefire-plugin"))
-				.forEach(p -> {
-					Xpp3Dom config = ofNullable(p.getConfiguration()).map(c -> (Xpp3Dom) c)
-							.orElseGet(() -> new Xpp3Dom("configuration"));
-					getLog().info(config.toString());
-					getLog().info("Class: " + config.getClass().getName());
-					Xpp3Dom argLine = new Xpp3Dom("argLine");
-					argLine.setValue("-Dtest.param=TestTest");
-					config.addChild(argLine);
-					getLog().info(config.toString());
-					p.setConfiguration(config);
-				});
+				.filter(p -> p.getId().startsWith(RP_PLUGIN_ID))
+				.map(p -> ofNullable(p.getConfiguration()).map(c -> (Xpp3Dom) c)
+						.orElseGet(() -> new Xpp3Dom("configuration")))
+				.flatMap(c -> Arrays.stream(c.getChildren()))
+				.filter(kv -> kv.getName().startsWith("rp."))
+				.collect(Collectors.toMap(Xpp3Dom::getName, Xpp3Dom::getValue)));
 
+		String propertyFilePathStr = pluginProperties.containsKey(PropertiesLoader.PROPERTIES_PATH_PROPERTY) ?
+				pluginProperties.getProperty(PropertiesLoader.PROPERTIES_PATH_PROPERTY) :
+				PropertiesLoader.getPropertyFilePath();
+		Path propertyFilePath = Paths.get(buildDirectoryPath, "classes", propertyFilePathStr);
+
+		Properties fsProperties = new Properties();
+		try {
+			fsProperties.load(Files.newInputStream(propertyFilePath));
+		} catch (FileNotFoundException e) {
+			// Override property file then
+		} catch (IOException e) {
+			throw new MojoExecutionException("Unable to read Report Portal property file", e);
+		}
+		fsProperties.putAll(pluginProperties);
+		try {
+			fsProperties.store(Files.newOutputStream(propertyFilePath), "Report Portal generated properties");
+		} catch (IOException e) {
+			throw new MojoExecutionException("Unable to write Report Portal property file", e);
+		}
+	}
+
+	private void generateJunit5files(){
+		// TODO: implement
+	}
+
+	private void generateTestNgFiles(){
+		// TODO: implement
+	}
+
+	@Override
+	public void execute() throws MojoExecutionException {
+		overrideProperties();
 	}
 }
