28f1b6956fcecd92506aaa8ff696fa5e812a25b3
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-12-27 18:38:02 +0300

    Add JUnit 5 setup

--- src/main/java/com/epam/reportportal/ReportPortalInstallMojo.java
+++ src/main/java/com/epam/reportportal/ReportPortalInstallMojo.java
@@ -1,6 +1,7 @@
 package com.epam.reportportal;
 
 import com.epam.reportportal.utils.properties.PropertiesLoader;
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -9,11 +10,13 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.stream.Collectors;
@@ -25,11 +28,19 @@ public class ReportPortalInstallMojo extends AbstractMojo {
 
 	private static final String RP_PLUGIN_ID = "com.epam.reportportal:reportportal-maven-plugin";
 
+	private static final String JUNIT5_EXTENSION_FILE_NAME = "org.junit.jupiter.api.extension.Extension";
+	private static final String JUNIT5_EXTENSION_FILE_DESTINATION_PATH = "META-INF/services";
+	private static final String JUNIT5_PROPERTY_FILE_NAME = "junit-platform.properties";
+	private static final String JUNIT5_EXTENSION_FILE_SOURCE_PATH = "services/" + JUNIT5_EXTENSION_FILE_NAME;
+
 	@Parameter(property = "session", defaultValue = "${session}", readonly = true)
 	private MavenSession session;
 
 	@Parameter(property = "project.build.directory", defaultValue = "${project.build.directory}", readonly = true)
 	private String buildDirectoryPath;
+
+	@Parameter(defaultValue = "${project.build.testOutputDirectory}")
+	private File testClassesDirectory;
 
 	private void overrideProperties() throws MojoExecutionException {
 		Properties pluginProperties = new Properties();
@@ -46,15 +57,17 @@ public class ReportPortalInstallMojo extends AbstractMojo {
 		String propertyFilePathStr = pluginProperties.containsKey(PropertiesLoader.PROPERTIES_PATH_PROPERTY) ?
 				pluginProperties.getProperty(PropertiesLoader.PROPERTIES_PATH_PROPERTY) :
 				PropertiesLoader.getPropertyFilePath();
-		Path propertyFilePath = Paths.get(buildDirectoryPath, "classes", propertyFilePathStr);
+		Path propertyFilePath = Paths.get(testClassesDirectory.getPath(), propertyFilePathStr);
 
 		Properties fsProperties = new Properties();
-		try {
-			fsProperties.load(Files.newInputStream(propertyFilePath));
-		} catch (FileNotFoundException e) {
-			// Override property file then
-		} catch (IOException e) {
-			throw new MojoExecutionException("Unable to read Report Portal property file", e);
+		if (propertyFilePath.toFile().exists()) {
+			try {
+				fsProperties.load(Files.newInputStream(propertyFilePath));
+			} catch (FileNotFoundException e) {
+				// Override property file then
+			} catch (IOException e) {
+				throw new MojoExecutionException("Unable to read Report Portal property file", e);
+			}
 		}
 		fsProperties.putAll(pluginProperties);
 		try {
@@ -64,16 +77,65 @@ public class ReportPortalInstallMojo extends AbstractMojo {
 		}
 	}
 
-	private void generateJunit5files() {
+	private static byte[] getFileContent(String path) throws MojoExecutionException {
+		byte[] extensionFileContent;
+		try {
+			extensionFileContent = IOUtils.toByteArray(ofNullable(Thread.currentThread()
+					.getContextClassLoader()
+					.getResourceAsStream(path)).orElseThrow(() -> new MojoExecutionException(
+					"Unable to find JUnit 5 file: " + path)));
+		} catch (IOException e) {
+			throw new MojoExecutionException("Unable to read JUnit 5 file: " + path, e);
+		}
+		return extensionFileContent;
+	}
+
+	private void setupJunit5() throws MojoExecutionException {
+		byte[] extensionFileContent = getFileContent(JUNIT5_EXTENSION_FILE_SOURCE_PATH);
+		byte[] propertyFileContent = getFileContent(JUNIT5_PROPERTY_FILE_NAME);
+		Path extensionFileDestinationPath = Paths.get(testClassesDirectory.getPath(),
+				JUNIT5_EXTENSION_FILE_DESTINATION_PATH,
+				JUNIT5_EXTENSION_FILE_NAME
+		);
+		Path propertyFileDestinationPath = Paths.get(testClassesDirectory.getPath(), JUNIT5_PROPERTY_FILE_NAME);
+		try {
+			if (extensionFileDestinationPath.toFile().exists()) {
+				Files.write(extensionFileDestinationPath, new byte[] { '\n' }, StandardOpenOption.APPEND);
+				Files.write(extensionFileDestinationPath, extensionFileContent, StandardOpenOption.APPEND);
+			} else {
+				extensionFileDestinationPath.toFile().getParentFile().mkdirs();
+				Files.write(extensionFileDestinationPath, extensionFileContent, StandardOpenOption.CREATE_NEW);
+			}
+		} catch (IOException e) {
+			throw new MojoExecutionException("Unable to write JUnit 5 extension file", e);
+		}
+		try {
+			if (!propertyFileDestinationPath.toFile().exists()) {
+				Files.write(propertyFileDestinationPath, propertyFileContent);
+			}
+		} catch (IOException e) {
+			throw new MojoExecutionException("Unable to write JUnit 5 property file", e);
+		}
+	}
+
+	private void setupTestNg() {
 		// TODO: implement
 	}
 
-	private void generateTestNgFiles() {
+	private void setupLogbackLogger() {
+		// TODO: implement
+	}
+
+	private void setupLog4jLogger() {
 		// TODO: implement
 	}
 
 	@Override
 	public void execute() throws MojoExecutionException {
 		overrideProperties();
+		setupJunit5();
+		setupTestNg();
+		setupLogbackLogger();
+		setupLog4jLogger();
 	}
 }
