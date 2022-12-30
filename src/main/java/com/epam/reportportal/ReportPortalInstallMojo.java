84e07f323963a26e31f63e51b2e15f47aac0926b
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-12-30 19:36:55 +0300

    Plugin update

--- src/main/java/com/epam/reportportal/ReportPortalInstallMojo.java
+++ src/main/java/com/epam/reportportal/ReportPortalInstallMojo.java
@@ -4,7 +4,7 @@ import com.epam.reportportal.utils.properties.PropertiesLoader;
 import org.apache.commons.io.IOUtils;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.execution.MavenSession;
-import org.apache.maven.model.Dependency;
+import org.apache.maven.model.ConfigurationContainer;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
@@ -12,7 +12,6 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.component.repository.ComponentDependency;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 import java.io.File;
@@ -22,9 +21,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.util.Optional.ofNullable;
@@ -33,8 +30,11 @@ import static java.util.Optional.ofNullable;
 public class ReportPortalInstallMojo extends AbstractMojo {
 
 	private static final String RP_DEPENDENCY_GROUP = "com.epam.reportportal";
-
 	private static final String RP_PLUGIN_ID = RP_DEPENDENCY_GROUP + ":reportportal-maven-plugin";
+
+	private static final Set<String> SUPPORTED_PLUGINS = new HashSet<>(Arrays.asList("org.apache.maven.plugins:maven-surefire-plugin",
+			"org.apache.maven.plugins:maven-failsafe-plugin"
+	));
 
 	private static final String JUNIT5_EXTENSION_FILE_NAME = "org.junit.jupiter.api.extension.Extension";
 	private static final String JUNIT5_EXTENSION_FILE_DESTINATION_PATH = "META-INF/services";
@@ -144,21 +144,39 @@ public class ReportPortalInstallMojo extends AbstractMojo {
 	}
 
 	public void addDependencies() {
-		List<ComponentDependency> pluginDependencies = pluginDescriptor.getDependencies();
-		getLog().debug("Report Portal Plugin dependencies: " + pluginDependencies);
-		List<Dependency> projectDependencies = project.getDependencies();
-		pluginDependencies.stream()
-				.filter(d -> RP_DEPENDENCY_GROUP.equals(d.getGroupId()))
-				.forEach(d -> {
-					getLog().debug("Adding dependency: " + d.getArtifactId());
-					Dependency prjDep = new Dependency();
-					prjDep.setScope("test");
-					prjDep.setGroupId(d.getGroupId());
-					prjDep.setArtifactId(d.getArtifactId());
-					prjDep.setVersion(d.getVersion());
-					prjDep.setType(d.getType());
-					projectDependencies.add(prjDep);
-				});
+		List<Artifact> pluginArtifacts = pluginDescriptor.getArtifacts();
+		List<Artifact> rpArtifact = pluginArtifacts.stream()
+				.filter(d -> RP_DEPENDENCY_GROUP.equals(d.getGroupId()) && !d.getArtifactId()
+						.equals(pluginDescriptor.getArtifactId()))
+				.collect(Collectors.toList());
+		getLog().debug("Report Portal Plugin artifacts: " + pluginArtifacts);
+		project.getBuildPlugins().stream().filter(p -> SUPPORTED_PLUGINS.contains(p.getKey())).forEach(plugin -> {
+			Xpp3Dom pluginConfig = plugin.getExecutions()
+					.stream()
+					.filter(e -> "test".equals(e.getPhase()))
+					.map(ConfigurationContainer::getConfiguration)
+					.findAny()
+					.map(cfg -> (Xpp3Dom) cfg)
+					.orElseGet(() -> {
+						Xpp3Dom xmlCfg = new Xpp3Dom("configuration");
+						plugin.setConfiguration(xmlCfg);
+						return xmlCfg;
+					});
+			Xpp3Dom additionalCpElements = ofNullable(pluginConfig.getChild("additionalClasspathElements")).orElseGet(() -> {
+				Xpp3Dom cpElements = new Xpp3Dom("additionalClasspathElements");
+				pluginConfig.addChild(cpElements);
+				return cpElements;
+			});
+			rpArtifact.forEach(a -> {
+				getLog().debug("Adding artifact: " + a.getId());
+				Xpp3Dom cpElement = new Xpp3Dom("additionalClasspathElement");
+				additionalCpElements.addChild(cpElement);
+				String path = a.getFile().getAbsolutePath();
+				getLog().debug("Artifact path: " + path);
+				cpElement.setValue(path);
+			});
+			getLog().info("Final config: " + pluginConfig);
+		});
 	}
 
 	@Override
