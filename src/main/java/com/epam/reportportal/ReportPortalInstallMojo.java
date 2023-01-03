0d6702b3285fbb88d3a2bc37a3d75679f400b1d9
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-01-03 18:21:13 +0300

    Plugin update

--- src/main/java/com/epam/reportportal/ReportPortalInstallMojo.java
+++ src/main/java/com/epam/reportportal/ReportPortalInstallMojo.java
@@ -1,10 +1,10 @@
 package com.epam.reportportal;
 
 import com.epam.reportportal.utils.properties.PropertiesLoader;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.execution.MavenSession;
-import org.apache.maven.model.ConfigurationContainer;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
@@ -14,27 +14,40 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
+import java.util.function.Predicate;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 
 import static java.util.Optional.ofNullable;
 
-@Mojo(name = "configure", defaultPhase = LifecyclePhase.TEST)
+@Mojo(name = "configure", defaultPhase = LifecyclePhase.GENERATE_TEST_RESOURCES)
 public class ReportPortalInstallMojo extends AbstractMojo {
 
 	private static final String RP_DEPENDENCY_GROUP = "com.epam.reportportal";
 	private static final String RP_PLUGIN_ID = RP_DEPENDENCY_GROUP + ":reportportal-maven-plugin";
+	private static final String TEST_NG_ID = "org.testng:testng";
+	private static final String JUNIT_5_ID = "org.junit.jupiter:junit-jupiter-api";
 
-	private static final Set<String> SUPPORTED_PLUGINS = new HashSet<>(Arrays.asList("org.apache.maven.plugins:maven-surefire-plugin",
-			"org.apache.maven.plugins:maven-failsafe-plugin"
-	));
+	private static final Predicate<Artifact> RP_DEPENDENCY = a -> a.getDependencyTrail().size() > 1
+			&& a.getDependencyTrail().get(1).startsWith(RP_DEPENDENCY_GROUP);
+	private static final Predicate<Artifact> TEST_NG_DEPENDENCY = a -> a.getDependencyTrail()
+			.stream()
+			.anyMatch(d -> d.startsWith(TEST_NG_ID));
+	private static final Predicate<Artifact> JUNIT_5_DEPENDENCY = a -> a.getDependencyTrail()
+			.stream()
+			.anyMatch(d -> d.startsWith(JUNIT_5_ID));
+	private static final Predicate<Artifact> JUNIT_5_OR_TEST_NG_DEPENDENCY = TEST_NG_DEPENDENCY.or(JUNIT_5_DEPENDENCY);
+	private static final Predicate<Artifact> NOT_JUNIT_5_OR_TEST_NG_DEPENDENCY = JUNIT_5_OR_TEST_NG_DEPENDENCY.negate();
 
 	private static final String JUNIT5_EXTENSION_FILE_NAME = "org.junit.jupiter.api.extension.Extension";
 	private static final String JUNIT5_EXTENSION_FILE_DESTINATION_PATH = "META-INF/services";
@@ -84,6 +97,7 @@ public class ReportPortalInstallMojo extends AbstractMojo {
 		}
 		fsProperties.putAll(pluginProperties);
 		try {
+			FileUtils.forceMkdir(propertyFilePath.getParent().toFile());
 			fsProperties.store(Files.newOutputStream(propertyFilePath), "Report Portal generated properties");
 		} catch (IOException e) {
 			throw new MojoExecutionException("Unable to write Report Portal property file", e);
@@ -143,40 +157,38 @@ public class ReportPortalInstallMojo extends AbstractMojo {
 		// TODO: implement
 	}
 
-	public void addDependencies() {
+	public void addDependencies() throws MojoExecutionException {
 		List<Artifact> pluginArtifacts = pluginDescriptor.getArtifacts();
 		List<Artifact> rpArtifact = pluginArtifacts.stream()
-				.filter(d -> RP_DEPENDENCY_GROUP.equals(d.getGroupId()) && !d.getArtifactId()
-						.equals(pluginDescriptor.getArtifactId()))
+				.filter(d -> !d.getArtifactId().equals(pluginDescriptor.getArtifactId()))
+				.filter(RP_DEPENDENCY)
+				.filter(NOT_JUNIT_5_OR_TEST_NG_DEPENDENCY)
 				.collect(Collectors.toList());
-		getLog().debug("Report Portal Plugin artifacts: " + pluginArtifacts);
-		project.getBuildPlugins().stream().filter(p -> SUPPORTED_PLUGINS.contains(p.getKey())).forEach(plugin -> {
-			Xpp3Dom pluginConfig = plugin.getExecutions()
-					.stream()
-					.filter(e -> "test".equals(e.getPhase()))
-					.map(ConfigurationContainer::getConfiguration)
-					.findAny()
-					.map(cfg -> (Xpp3Dom) cfg)
-					.orElseGet(() -> {
-						Xpp3Dom xmlCfg = new Xpp3Dom("configuration");
-						plugin.setConfiguration(xmlCfg);
-						return xmlCfg;
-					});
-			Xpp3Dom additionalCpElements = ofNullable(pluginConfig.getChild("additionalClasspathElements")).orElseGet(() -> {
-				Xpp3Dom cpElements = new Xpp3Dom("additionalClasspathElements");
-				pluginConfig.addChild(cpElements);
-				return cpElements;
-			});
-			rpArtifact.forEach(a -> {
-				getLog().debug("Adding artifact: " + a.getId());
-				Xpp3Dom cpElement = new Xpp3Dom("additionalClasspathElement");
-				additionalCpElements.addChild(cpElement);
-				String path = a.getFile().getAbsolutePath();
-				getLog().debug("Artifact path: " + path);
-				cpElement.setValue(path);
-			});
-			getLog().info("Final config: " + pluginConfig);
-		});
+		getLog().debug("Report Portal Plugin artifacts: " + rpArtifact);
+		for (Artifact a : rpArtifact) {
+			getLog().debug("Adding artifact: " + a.getId());
+			File sourceFile = a.getFile();
+			try (JarFile jar = new JarFile(sourceFile)) {
+				Enumeration<JarEntry> jarEntries = jar.entries();
+				while (jarEntries.hasMoreElements()) {
+					JarEntry entry = jarEntries.nextElement();
+					File destFile = Paths.get(testClassesDirectory.getPath(), entry.getName()).toFile();
+					if (destFile.exists()) {
+						continue;
+					}
+					if (entry.isDirectory()) {
+						FileUtils.forceMkdir(destFile);
+						continue;
+					}
+					try (InputStream input = jar.getInputStream(entry);
+							FileOutputStream output = new FileOutputStream(destFile)) {
+						IOUtils.copy(input, output);
+					}
+				}
+			} catch (IOException e) {
+				throw new MojoExecutionException("Unable to open jar file: " + sourceFile.getAbsolutePath());
+			}
+		}
 	}
 
 	@Override
